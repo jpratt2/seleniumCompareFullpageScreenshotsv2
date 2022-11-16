be75cec4b38f276cfe3386482929f5d274f5b68c
John Pratt <jpratt@adobe.com>
2022-11-16 12:02:05 -0700

    add URLs for Acrobat testing

--- src/test/java/screenshots/URLs.java
+++ src/test/java/screenshots/URLs.java
@@ -3,7 +3,9 @@ public class URLs {
 
     public static String[] list = {
   
-	"https://adobe.com/acrobat.html",
+	"https://www.adobe.com/acrobat/business.html?tt=DC0917-1",
+
+
 
 
 
@@ -11,6 +13,48 @@ public class URLs {
 	
 
 	public static String[] listTwiceLoaded = {
+
+//802
+"https://www.adobe.com/acrobat.html?at_preview_token=DJF2qJ08ME_mXB_sA-V5VA&at_preview_index=1_3&at_preview_listed_activities_only=false&tt=prsnlz",
+"https://www.adobe.com/acrobat/pricing.html?at_preview_token=U7uj5unbnXiTvzfPQnxd7A&at_preview_index=1_2&at_preview_listed_activities_only=false&tt=prsnlz",
+"https://www.adobe.com/acrobat/free-trial-download.html?at_preview_token=SYWVvwDDVNMfeJ7c0DwQew&at_preview_index=1_3&at_preview_listed_activities_only=false&tt=prsnlz",
+ //677
+"https://www.adobe.com/acrobat.html?at_preview_token=DJF2qJ08ME_mXB_sA-V5VA&at_preview_index=1_1&at_preview_listed_activities_only=false&akamaiLocale=US&countryCode=US&mboxOverride.browserIp=11.255.255.254&tt=prsnlz",
+"https://www.adobe.com/acrobat/complete-pdf-solution.html?ttid=annual-pro&at_preview_token=CAFm7Z889xEam-VOl-uYcw&at_preview_index=1_1&at_preview_listed_activities_only=false&akamaiLocale=US&countryCode=US&mboxOverride.browserIp=11.255.255.254&tt=prsnlz",
+"https://www.adobe.com/acrobat/pdf-reader.html?at_preview_token=E52LLKyQ0ThOgXpMHyEKcg&at_preview_index=1_1&at_preview_listed_activities_only=false&akamaiLocale=US&countryCode=US&mboxOverride.browserIp=11.255.255.254&tt=prsnlz",
+"https://www.adobe.com/acrobat/free-trial-download.html?at_preview_token=SYWVvwDDVNMfeJ7c0DwQew&at_preview_index=1_1&at_preview_listed_activities_only=false&akamaiLocale=US&countryCode=US&mboxOverride.browserIp=11.255.255.254&tt=prsnlz",
+"https://www.adobe.com/?at_preview_token=3tc6J9v5Qcw8xld9XScKQw&at_preview_index=1_1&at_preview_listed_activities_only=true&test=target",
+"https://www.adobe.com/au/acrobat.html?at_preview_token=yrcHCAhhMskFuku1NhOgsg&at_preview_index=1_1&at_preview_listed_activities_only=false&akamaiLocale=AU&countryCode=AU&mboxOverride.browserIp=1.0.0.255&tt=prsnlz",
+"https://www.adobe.com/au/acrobat/complete-pdf-solution.html?ttid=annual-pro&at_preview_token=B_ukC6GKwMWWLZmsPOzoIw&at_preview_index=1_1&at_preview_listed_activities_only=false&akamaiLocale=AU&countryCode=AU&mboxOverride.browserIp=1.0.0.255&tt=prsnlz",
+"https://www.adobe.com/au/acrobat/pdf-reader.html?at_preview_token=1YGarL_xyzVgjhFo1twkpA&at_preview_index=1_1&at_preview_listed_activities_only=false&akamaiLocale=AU&countryCode=AU&mboxOverride.browserIp=1.0.0.255&tt=prsnlz",
+"https://www.adobe.com/au/acrobat/free-trial-download.html?at_preview_token=VJQoPuEKEJqonblJWxR8YA&at_preview_index=1_1&at_preview_listed_activities_only=false&akamaiLocale=AU&countryCode=AU&mboxOverride.browserIp=1.0.0.255&tt=prsnlz",
+"https://www.adobe.com/au/?at_preview_token=tSsGBnh9kgpWqfspJiCzOw&at_preview_index=1_1&at_preview_listed_activities_only=true&akamaiLocale=AU&mboxOverride.browserIp=1.0.0.255",
+//887
+"https://www.adobe.com/sign.html?at_preview_token=jGLn6vAkDrwhZrocVhLykA&at_preview_index=1_1",
+
+//907
+"https://www.adobe.com/acrobat/pdf-reader.html?tt=DC0907&at_preview_token=2j8g-d5u1SKIFdoU6rrnmA&at_preview_index=1_2&at_preview_listed_activities_only=false&at_preview_evaluate_as_true_audience_ids=2044873",
+
+//910
+"https://www.adobe.com/sign/free-trial-global.html?tt=DC0910&at_preview_token=XU9_G6_pPnDHwvvfgGlfCA&at_preview_index=1_2&at_preview_listed_activities_only=true&at_preview_evaluate_as_true_audience_ids=2054044",
+"https://www.adobe.com/sign/pricing/plans.html?tt=DC0910&at_preview_token=kiFo_ev4j44UU4VXvAlSbA&at_preview_index=1_2&at_preview_listed_activities_only=true&at_preview_evaluate_as_true_audience_ids=2054036",
+
+//899
+	"https://www.adobe.com/uk/acrobat/pricing.html?tt=DC0899&at_preview_token=ndKCDDjKvf44SDZ5GHbUmQ&at_preview_index=1_2&at_preview_listed_activities_only=false&akamaiLocale=GB&countryCode=GB&mboxOverride.browserIp=5.10.159.255",
+	"https://www.adobe.com/uk/acrobat/pricing/business.html?tt=DC0899&at_preview_token=ndKCDDjKvf44SDZ5GHbUmQ&at_preview_index=1_2&at_preview_listed_activities_only=false&akamaiLocale=GB&countryCode=GB&mboxOverride.browserIp=5.10.159.255",
+	"https://www.adobe.com/uk/acrobat/pricing/students.html?tt=DC0899&at_preview_token=ndKCDDjKvf44SDZ5GHbUmQ&at_preview_index=1_2&at_preview_listed_activities_only=false&akamaiLocale=GB&countryCode=GB&mboxOverride.browserIp=5.10.159.255",
+	"https://www.adobe.com/uk/acrobat/pricing/enterprise.html?tt=DC0899&at_preview_token=ndKCDDjKvf44SDZ5GHbUmQ&at_preview_index=1_2&at_preview_listed_activities_only=false&akamaiLocale=GB&countryCode=GB&mboxOverride.browserIp=5.10.159.255",
+	"https://www.adobe.com/uk/sign/pricing/plans.html?tt=DC0899&at_preview_token=ndKCDDjKvf44SDZ5GHbUmQ&at_preview_index=1_2&at_preview_listed_activities_only=false&akamaiLocale=GB&countryCode=GB&mboxOverride.browserIp=5.10.159.255",
+	"https://www.adobe.com/uk/sign/pricing/plans.html?plans=teams&tt=DC0899&at_preview_token=ndKCDDjKvf44SDZ5GHbUmQ&at_preview_index=1_2&at_preview_listed_activities_only=false&akamaiLocale=GB&countryCode=GB&mboxOverride.browserIp=5.10.159.255",
+
+	"https://www.adobe.com/ca/acrobat/pricing.html?tt=DC0899&at_preview_token=cjhEMZHtT5kPsRauMNQfQQ&at_preview_index=1_2&at_preview_listed_activities_only=true&at_preview_evaluate_as_true_audience_ids=2062413&akamaiLocale=CA&countryCode=CA&mboxOverride.browserIp=23.91.159.255",
+	"https://www.adobe.com/ca/acrobat/pricing/business.html?tt=DC0899&at_preview_token=cjhEMZHtT5kPsRauMNQfQQ&at_preview_index=1_2&at_preview_listed_activities_only=true&at_preview_evaluate_as_true_audience_ids=2062413&akamaiLocale=CA&countryCode=CA&mboxOverride.browserIp=23.91.159.255",
+	"https://www.adobe.com/ca/acrobat/pricing/students.html?tt=DC0899&at_preview_token=cjhEMZHtT5kPsRauMNQfQQ&at_preview_index=1_2&at_preview_listed_activities_only=true&at_preview_evaluate_as_true_audience_ids=2062413&akamaiLocale=CA&countryCode=CA&mboxOverride.browserIp=23.91.159.255",
+	"https://www.adobe.com/ca/acrobat/pricing/enterprise.html?tt=DC0899&at_preview_token=cjhEMZHtT5kPsRauMNQfQQ&at_preview_index=1_2&at_preview_listed_activities_only=true&at_preview_evaluate_as_true_audience_ids=2062413&akamaiLocale=CA&countryCode=CA&mboxOverride.browserIp=23.91.159.255",
+	"https://www.adobe.com/ca/sign/pricing/plans.html?tt=DC0899&at_preview_token=cjhEMZHtT5kPsRauMNQfQQ&at_preview_index=1_2&at_preview_listed_activities_only=true&at_preview_evaluate_as_true_audience_ids=2062413&akamaiLocale=CA&countryCode=CA&mboxOverride.browserIp=23.91.159.255",
+	"https://www.adobe.com/ca/sign/pricing/plans.html?plans=teams&tt=DC0899&at_preview_token=cjhEMZHtT5kPsRauMNQfQQ&at_preview_index=1_2&at_preview_listed_activities_only=true&at_preview_evaluate_as_true_audience_ids=2062413&akamaiLocale=CA&countryCode=CA&mboxOverride.browserIp=23.91.159.255",
+
+
 
 
 	};
