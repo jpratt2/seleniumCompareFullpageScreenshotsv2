608a066b8a73c2b0b019d151cd9c217a71cc8612
John Pratt <jpratt2@gmail.com>
2022-11-16 15:05:27 -0700

    Update C_retestTest.java
    fix C file for Setup extension

--- src/test/java/screenshots/C_retestTest.java
+++ src/test/java/screenshots/C_retestTest.java
@@ -2,13 +2,13 @@ package screenshots;
 import org.junit.Test;
 
 
-public class C_retestTest {
+public class C_retestTest extends Setup {
     @Test
-    public void _01_testAgainstBaseline() throws InterruptedException {
+    public void _01_testAgainstBaseline() {
 		//Same as A_ except it is using the "retest" list from the URLs file
 
 		Library.UrlLoop(URLs.retest,Library.getScreenshotMethod());
-        Library.UrlLoopTwiceLoaded(URLs.retestTwiceLoaded,Library.getScreenshotMethod());
+        	Library.UrlLoopTwiceLoaded(URLs.retestTwiceLoaded,Library.getScreenshotMethod());
 
     }
 
